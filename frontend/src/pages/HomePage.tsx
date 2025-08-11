b54595359387f4560b90f26e01e26d5f38edeacf
Stefano <s.vallodoro@datawizard.it>
2025-08-11 10:12:08 +0200

    Update HomePage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -10,10 +10,8 @@ const HomePage: React.FC = () => {
   return (
     <div className="h-full flex flex-col items-center justify-center bg-hero-bg text-center p-4">
       <img src={gravitateLogo} className='object-cover mb-8'></img>
-      <div>
-        <h1 className='text-white font-semibold uppercase tracking-widest mb-8 text-3xl'>{t('homePage.title')}</h1>
-        <p className='text-gray-300 max-w-2xl italic text-2xl'>{t('homePage.subtitle')}</p>
-      </div>
+      <h1 className='text-white font-normal uppercase tracking-widest mb-12 text-3xl'>{t('homePage.title')}</h1>
+      <p className='font-light text-gray-300 max-w-[50rem] italic text-2xl'>{t('homePage.subtitle')}</p>
     </div>
   );
 };
