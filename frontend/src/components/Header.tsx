46704c038c830217f94025f50e03f30a30c9ad72
Stefano <s.vallodoro@datawizard.it>
2025-08-11 09:46:01 +0200

    Update Header: centered elements in the header bar; bolded hamburger menu; white logo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
-import gravitateLogo from '../assets/gravitate-logo.png';
+import gravitateLogo from '../assets/gravitate-logo-w.png';
 import { HeaderProps } from '../utils/types';
 
 const Header: React.FC<HeaderProps> = ({ toggleSidebar }) => {
@@ -11,16 +11,21 @@ const Header: React.FC<HeaderProps> = ({ toggleSidebar }) => {
 
   return (
     <header className="bg-hero-bg">
-      <div className="container mx-auto px-4 py-6 flex justify-between items-center">
-        <div className='flex items-center space-x-4'>
-          <Link to="/"><img src={gravitateLogo} alt="Gravitate Health logo" className='h-10'></img></Link>
-          <h1 className='text-2xl font-bold text-white'>{t('header.title')}</h1>
+      <div className="mx-12 py-6 flex items-center justify-between">
+        <div className="flex-1 flex justify-start">
+          <Link to="/">
+            <img src={gravitateLogo} alt="Gravitate Health logo" className="h-10" />
+          </Link>
         </div>
-        <div className="flex items-center space-x-4">
-          {/* Hamburger */}
-          <button onClick={toggleSidebar} className='text-white focus:outline-none' aria-label='Open sidebar'>
-            <svg className='w-8 h-8' fill='none' stroke="currentColor" viewBox='0 0 24 24'>
-              <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+        <div className="flex-none">
+          <h1 className="text-2xl font-bold text-white whitespace-nowrap">  
+            {t('header.title')}
+          </h1>
+        </div>
+        <div className="flex-1 flex justify-end">
+          <button onClick={toggleSidebar} className="text-white focus:outline-none" aria-label="Open sidebar">
+            <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2.5} d="M4 6h16M4 12h16M4 18h16" />
             </svg>
           </button>
         </div>
